b6c0cd4e0986c40f65ad1b74d2c857ddf11e1ab5
Antony D'Amico <adamico@hispatecanalytics.com>
2021-05-14 23:37:07 +0200

    Linter fixes

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,14 +7,14 @@ from django.test import TestCase
 class CommandTests(TestCase):
 
     def test_wait_for_db_ready(self):
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
-            get_item.return_value = True
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_i:
+            get_i.return_value = True
             call_command('wait_for_db')
-            self.assertEqual(get_item.call_count, 1)
+            self.assertEqual(get_i.call_count, 1)
 
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, time_sleep):
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
-            get_item.side_effect = [OperationalError] * 5 + [True]
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_i:
+            get_i.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
-            self.assertEqual(get_item.call_count, 6)
+            self.assertEqual(get_i.call_count, 6)
